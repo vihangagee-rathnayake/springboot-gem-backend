714186d4a675f1c906381234b91dac0ddc4c2489
vihangagee-rathnayake <vihangageerathnayake@gmail.com>
2026-05-20 15:44:14 +0530

    update

--- src/main/java/com/srilankagem/gembackend/common/exception/ResourceNotFoundException.java
+++ src/main/java/com/srilankagem/gembackend/common/exception/ResourceNotFoundException.java
@@ -3,6 +3,7 @@ package com.srilankagem.gembackend.common.exception;
 public class ResourceNotFoundException extends Exception{
 
     public ResourceNotFoundException(String resource, String field){
+        super(String.format("Resource Not Found - %s with field - %s", resource, field)); // similar like python f
 
 
     }
